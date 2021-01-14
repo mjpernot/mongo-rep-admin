c38e5e1ce8e2243cb6682b086aa8106cfb772918
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-01-14 14:51:00 +0000

    Updated test/unit/mongo_rep_admin/_process_std.py v3.5.0

--- test/unit/mongo_rep_admin/_process_std.py
+++ test/unit/mongo_rep_admin/_process_std.py
@@ -138,6 +138,7 @@ class UnitTest(unittest.TestCase):
         self.conn2 = (False, "Error Message")
         self.status = (True, None)
         self.status2 = (False, "_process_std: Error Message")
+        self.db_tbl = "db:tbl"
 
     @mock.patch("mongo_rep_admin.mongo_libs.ins_doc")
     def test_mongo_failure(self, mock_mongo):
@@ -154,7 +155,7 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(
             mongo_rep_admin._process_std(
-                self.outdata, class_cfg="mongocfg", db_tbl="db:tbl",
+                self.outdata, class_cfg="mongocfg", db_tbl=self.db_tbl,
                 args_array=self.args_array2, suf=self.primary), self.status2)
 
     @mock.patch("mongo_rep_admin.mongo_libs.ins_doc")
@@ -172,7 +173,7 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(
             mongo_rep_admin._process_std(
-                self.outdata, class_cfg="mongocfg", db_tbl="db:tbl",
+                self.outdata, class_cfg="mongocfg", db_tbl=self.db_tbl,
                 args_array=self.args_array2, suf=self.primary), self.status)
 
     def test_std_out(self):
@@ -221,7 +222,7 @@ class UnitTest(unittest.TestCase):
 
         self.assertEqual(
             mongo_rep_admin._process_std(
-                self.outdata, class_cfg="mongocfg", db_tbl="db:tbl",
+                self.outdata, class_cfg="mongocfg", db_tbl=self.db_tbl,
                 args_array=self.args_array2, suf=self.primary), self.status)
 
     @mock.patch("mongo_rep_admin.gen_libs.openfile",
